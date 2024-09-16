317719492236decd319fa8b7fd489dd98f89e500
Rosefinch-Midsummer <que2213254082@gmail.com>
2024-09-16 19:39:32 +0800

    4th part socket 90% completed

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
@@ -22,7 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,12 +1,12 @@
-// import { useSocketContext } from "../../context/SocketContext";
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
-	// const { onlineUsers } = useSocketContext();
-	// const isOnline = onlineUsers.includes(conversation._id);
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
 
 	return (
 		<>
@@ -15,9 +15,9 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 				${isSelected ? "bg-sky-500" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
-			>
-				{/* <div className={`avatar ${isOnline ? "online" : ""}`}> */}
-				<div className={`avatar online}`}>
+			>	
+				{console.log("isOnline = ", isOnline)}
+				<div className={`avatar ${isOnline ? "online" : ""}`}>
 					<div className='w-12 rounded-full'>
 						<img src={conversation.profilePic} alt='user avatar' />
 					</div>
